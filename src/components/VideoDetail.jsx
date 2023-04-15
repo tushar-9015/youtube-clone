459fe6ff2d9c17be9573ab8371f99a03bc8126b9
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-04-15 22:04:19 +0530

    commit

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+  const { id } = useParams(); 
+
+  useEffect(() =>{
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then((data) => setVideoDetail(data.items[0]));
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+      .then((data) => setVideos(data.items))
+  },[id]);
+
+
+const snippet = videoDetail?.snippet ?? {title: '', channelId: '', channelTitle: ''}
+
+const {title, channelId, channelTitle} = snippet
+
+  
+
   return (
-    <div>
-      gsdg
-    </div>
+    <Box minHeight='95vh '>
+      <Stack direction={{ xs: 'column', md: 'row'}}>
+        <Box flex={1}>
+          <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} 
+            className='react-player' controls />
+            <Typography color='white'>
+              {title}
+            </Typography>
+            <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
+              <Link to={`/channel/${channelId}`}>
+                <Typography variant={{ sm: "subtitle1", md: 'h6' }}  color="#fff" >
+                  {channelTitle}
+                  <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+                </Typography>
+              </Link>
+              </Stack>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
