2ab920c4d3d512f69cb4f1a0ad0d744d0750141e
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-04-15 23:15:59 +0530

    commit

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Box } from '@mui/material';
-import { ChannelCard, Videos } from './';
-import { fetchFromAPI } from '../utils/fetchFromAPI';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  const [channelDetail, setChannelDetail] = useState(null);
-  const [videos, setVideos] = useState([]);
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
+
   const { id } = useParams();
 
-  console.log(channelDetail);
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
 
-  useEffect(()=> {
-    fetchFromAPI(`channels?part=snippet&id=${id}`)
-        .then((data) => setChannelDetail(data?.items[0]));
-    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
-        .then((data) => setVideos(data?.items));
-  },[id])
+      setChannelDetail(data?.items[0]);
+
+      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
+  }, [id]);
+
   return (
     <Box minHeight="95vh">
       <Box>
@@ -33,7 +40,7 @@ const ChannelDetail = () => {
         <Videos videos={videos} />
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default ChannelDetail
+export default ChannelDetail;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,53 +1,45 @@
-import React from 'react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Paper, IconButton } from '@mui/material'
-import { Search } from '@mui/icons-material';
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
-
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-     e.preventDefault();
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
 
-     if(searchTerm){
+    if (searchTerm) {
       navigate(`/search/${searchTerm}`);
 
       setSearchTerm('');
-     }
+    }
+  };
 
-  }
   return (
     <Paper
       component='form'
-      onSubmit={handleSubmit}
-      sx={{ 
+      onSubmit={onhandleSubmit}
+      sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
         pl: 2,
         boxShadow: 'none',
-        mr: { sm: 5 }
+        mr: { sm: 5 },
       }}
     >
-      <input 
+      <input
         className='search-bar'
         placeholder='Search...'
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <IconButton 
-        type='submit'
-        sx={{ 
-          p: '10px',
-          color: 'red'
-        }}
-      >
-        <Search />
+      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+        <SearchIcon />
       </IconButton>
     </Paper>
-  )
-}
+  );
+};
 
 export default SearchBar;
